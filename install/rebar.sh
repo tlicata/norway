709429434d402d70fd2a1a5bcbf83e0ae0500c9e
Tim Licata <tim@timothylicata.com>
2016-06-18 22:58:35 -0700

    install lfe templates for rebar

--- install/rebar.sh
+++ install/rebar.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Install rebar
 if [ ! -d rebar3 ]; then
     git clone https://github.com/erlang/rebar3.git
     cd rebar3
@@ -8,4 +9,15 @@ if [ ! -d rebar3 ]; then
     echo "" >> ~/.bashrc
     echo "# Add rebar to path" >> ~/.bashrc
     echo "export PATH=$PATH:/home/vagrant/.cache/rebar3/bin" >> ~/.bashrc
+fi
+
+# Install LFE templates
+TEMPLATE_DIR=~/.config/rebar3/templates
+if [ ! -d rebar3_lfe_templates ]; then
+    git clone https://github.com/arpunk/rebar3_lfe_templates.git
+    mkdir -p $TEMPLATE_DIR
+    cd rebar3_lfe_templates
+    cp lfe_app/* $TEMPLATE_DIR/
+    cp lfe_lib/* $TEMPLATE_DIR/
+    cp lfe_release/* $TEMPLATE_DIR/
 fi
